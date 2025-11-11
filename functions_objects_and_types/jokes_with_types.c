72c7d01b080dd9ba63d80e9f62c02e97e99f7e66
LLIypKa <shilkoaa@gmail.com>
2025-11-11 22:04:17 +0700

    more about functions and types

--- functions_objects_and_types/jokes_with_types.c
+++ functions_objects_and_types/jokes_with_types.c
@@ -1,10 +1,25 @@
+//задание для NAGGER - подумай, какой вывод будет у вывода перечислений
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <locale.h>
 #include <wchar.h>
+#define month_count 12
+
+enum months {
+    jan = 1,
+    feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+};
+
+enum months_v2 {
+    jan_v2, feb_v2, mar_v2, apr_v2, may_v2, jun_v2, jul_v2, aug_v2, sep_v2, oct_v2, nov_v2, dec_v2, months_count
+};
 
 int main(void) {
+    enum months months[] = { jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec };
+    enum months_v2 months_v2[] = { jan_v2, feb_v2, mar_v2, apr_v2, may_v2, jun_v2, jul_v2, aug_v2, sep_v2, oct_v2, nov_v2, dec_v2, months_count };
+
     setlocale(LC_ALL, "");
     //Оба варианта допустимы
     _Bool boolean = 1;
@@ -17,5 +32,21 @@ int main(void) {
     wprintf(L"%lc\n", t);
     wprintf(L"%ls\n", message);
 
+    for (int i = 0; i < month_count; i++) {
+        wprintf(L"%d\t", months[i]);
+    }
+
+    wprintf(L"\n");
+
+    for (int i = 0; i < months_count; i++) {
+        wprintf(L"%d\t", months_v2[i]);
+    }
+
+    wprintf(L"\n");
+
     return EXIT_SUCCESS;
+}
+
+void g(int j) {
+    
 }
