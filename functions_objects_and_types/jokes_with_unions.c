fed705c5278ef0c215b4d0e0b198e0d93bcceaf4
LLIypKa <shilkoaa@gmail.com>
2025-11-13 22:05:04 +0700

    unions

---
+++ functions_objects_and_types/jokes_with_unions.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+union BadPiggiesWithBrokenHouses
+{
+    struct NafNaf {
+        bool has_house;
+        int guests_in_house;
+    } naf_naf;
+
+    struct NifNif {
+        bool has_house;
+        double saved_bricks_from_house;
+    } nif_nif;
+
+    struct NufNuf {
+        bool has_house;
+        int met_wolf_count;
+    } nuf_nuf;
+};
+
+union ascii {
+    int digit;
+    char letter;
+};
+
+int main(void) {
+    union BadPiggiesWithBrokenHouses piggies;
+    union BadPiggiesWithBrokenHouses* piggies_ptr = &piggies;
+
+    piggies.naf_naf.has_house = true;
+    piggies.naf_naf.guests_in_house = 2;
+
+    //теперь Наф-нафа данные будут перезаписаны
+    piggies_ptr->nif_nif.has_house = false;
+    piggies_ptr->nif_nif.saved_bricks_from_house = 3.5;
+
+    //теперь наф-наф аткивен, а ниф-ниф идёт считать кирпичи дальше
+    printf("Guests in NAF-NAF's house count: %d\n", piggies_ptr->naf_naf.guests_in_house);
+
+    piggies_ptr->nuf_nuf.met_wolf_count = 3;
+    piggies_ptr->nuf_nuf.has_house = false;
+
+    //теперь наф-наф аткивен, а нуф-нуф погнал волков гонять
+    printf("Guests in NAF-NAF's house count: %d\n", piggies_ptr->naf_naf.guests_in_house);
+
+    //почему вывод разный?
+    /*
+    на примере объдинения ascii
+
+    памяти будет выделено по максимальному члену объединения. 
+    В общем случае - это будет выглядеть так (char - 1 байт, int - 4 байта): (одна клетка - один байт)
+    ________________________
+    |     int   |     di   |   gi     |    t     |
+    |            |           |           |          |
+    |  char   |           |           |          |
+    | letter  |           |           |          |
+    |-----------------------------------|
+
+    В перовом байте будет болтаться символьное значение, и все 4 могут использоваться для целочисленного значения
+
+    Если создать переменную объединения, например так:
+    union ascii code;
+    то, так как оба элемента - letter и digit занимают одну и ту же память, то данные фактически одни и те же,
+    только при обращении к code.digit данные интерпретируются как объект int, 
+    а при обращении к code.letter - как объект char.
+    И изменение одного из них приведет к изменению другого.
+
+    code.digit = 120;
+    printf("%d - %c \n", code.digit, code.letter);  // 120 - x
+    printf("%d - %d \n", code.digit, code.letter);  // 120 - 120
+
+    code.letter = 87;
+    printf("%d - %c \n", code.digit, code.letter);  // 87 - W
+    */
+
+    return EXIT_SUCCESS;
+}
